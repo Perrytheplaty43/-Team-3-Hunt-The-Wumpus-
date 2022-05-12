cb17d5a4a44d377323057cd0190b025a2ba23eb0
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-12 14:39:16 -0700

    made a change

--- Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/Form1.cs
+++ Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/Form1.cs
@@ -17,5 +17,9 @@ namespace Team_3_Hunt_The_Wumpus
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //change
+        }
     }
 }
