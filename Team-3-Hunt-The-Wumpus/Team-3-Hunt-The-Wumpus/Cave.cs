13f9494b9fb4535d734cbd89596116fb7c4487ae
connorno <105462039+connorno@users.noreply.github.com>
2022-05-16 15:12:41 -0700

    Update Cave.cs

--- Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/Cave.cs
+++ Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/Cave.cs
@@ -8,5 +8,9 @@ namespace Team_3_Hunt_The_Wumpus
 {
     class Cave
     {
+        public string[] connectedRooms = new string[30];
+        public string[] adjacentRooms = new string[30];        
+        
+        //room class
     }
 }
