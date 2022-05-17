e14561bc5e326414e314bcb6045bfc42f8fefc68
melundeath <105462059+melundeath@users.noreply.github.com>
2022-05-17 15:02:54 -0700

    Update to GameLocation

--- Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/GameLocation.cs
+++ Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/GameLocation.cs
@@ -6,53 +6,91 @@ using System.Threading.Tasks;
 
 namespace Team_3_Hunt_The_Wumpus
 {
-    class GameLocation
+    public class GameLocation
     {
         // instance variables
+        // whenever the player moves forward, PlayerLocation should be updated
         public int PlayerLocation { get; set; }
         public int WumpusLocation { get; set; }
-        public int PitsLocation { get; set; }
-        public int BatsLocation { get; set; }
+        public int Pit1Location { get; set; }
+        public int Pit2Location { get; set; }
+        public int Bat1Location { get; set; }
+        public int Bat2Location { get; set; }
+
+        Random rndRoom = new Random();
 
         // constructor
-        public GameLocation(int pL, int wL, int piL, int bL)
+        public GameLocation(int pL, int wL, int piL1, int pil2, int bL1, int bl2)
         {
             pL = PlayerLocation;
             wL = WumpusLocation;
-            piL = PitsLocation;
-            bL = BatsLocation;
+            piL1 = Pit1Location;
+            pil2 = Pit2Location;
+            bL1 = Bat1Location;
+            bl2 = Bat2Location;
         }
 
         // functions
-        // randomizes player location and updates
+        // randomizes player location 
         public void RandomizePlayerLocation()
         {
-            PlayerLocation = 0;
+            int room;
+
+            room = rndRoom.Next(30);
+            PlayerLocation = room;
         }
 
-        // randomizes wumpus location to 2-4 rooms away and updates
+        // randomizes wumpus location to 2-4 rooms away from its current location
         public void RandomizeWumpusLocation()
         {
-            WumpusLocation = 0;
+            int room;
+
+            room = rndRoom.Next(4);
+            WumpusLocation = WumpusLocation + room;
         }
 
-        // randomizes the locations of both pit rooms and updates
+        // randomizes the locations of both pit rooms 
         public void RandomizePitsLocation()
         {
-            PitsLocation = 0;
+            int room1, room2;
+            
+            room1 = rndRoom.Next(30);
+            room2 = rndRoom.Next(30);
+            Pit1Location = room1;
+            Pit2Location = room2;
         }
 
-        // randomizes the locations of both bat rooms and updates
+        // randomizes the locations of both bat rooms 
         public void RandomizeBatsLocation()
         {
-            BatsLocation = 0;
+            int room1, room2;
+
+            room1 = rndRoom.Next(30);
+            room2 = rndRoom.Next(30);
+            Bat1Location = room1;
+            Bat2Location = room2;
         }
 
         // returns a room-specific warning
         public string GiveWarning()
         {
             // different outcome based on which room player is near
-            
+            if ()
+            {
+                // wumpus
+                return "I smell a Wumpus!";
+            }
+            else if ()
+            {
+                // pits
+                return "Bats nearby!";
+            }
+            else if ()
+            {
+                // bats
+                return "I feel a draft!";
+            }
+
             return "";
         }
 
@@ -62,10 +100,11 @@ namespace Team_3_Hunt_The_Wumpus
             // different outcome based on whether the arrow hits wumpus
             if (WumpusLocation == arrowLocation)
             {
-
+                // end game
             }
             else
             {
+                // wumpus changes room
                 RandomizeWumpusLocation();
             }
         }
@@ -73,7 +112,7 @@ namespace Team_3_Hunt_The_Wumpus
         // returns a random piece of trivia
         public string GetHint()
         {
-            return "";
+            return ""; 
         }
     }
 }
