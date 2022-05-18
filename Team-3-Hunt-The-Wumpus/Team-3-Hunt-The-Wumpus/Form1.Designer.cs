987e433e51eff2b760a35af706ad6547a58dfe97
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-17 23:20:10 -0700

    Update Form1.Designer.cs

--- Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/Form1.Designer.cs
+++ Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Team_3_Hunt_The_Wumpus
         {
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxCommand = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
@@ -51,19 +52,30 @@ namespace Team_3_Hunt_The_Wumpus
             this.textBoxCommand.BackColor = System.Drawing.Color.Black;
             this.textBoxCommand.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxCommand.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBoxCommand.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.textBoxCommand.ForeColor = System.Drawing.Color.Lime;
-            this.textBoxCommand.Location = new System.Drawing.Point(0, 613);
+            this.textBoxCommand.Location = new System.Drawing.Point(0, 579);
             this.textBoxCommand.Name = "textBoxCommand";
             this.textBoxCommand.PlaceholderText = "enter command...";
-            this.textBoxCommand.Size = new System.Drawing.Size(1304, 16);
+            this.textBoxCommand.Size = new System.Drawing.Size(1304, 50);
             this.textBoxCommand.TabIndex = 1;
             this.textBoxCommand.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 134);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1304, 445);
+            this.panel1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1304, 629);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBoxCommand);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -78,6 +90,7 @@ namespace Team_3_Hunt_The_Wumpus
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBoxCommand;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
