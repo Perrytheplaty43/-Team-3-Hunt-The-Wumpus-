1bfeb1351a5bd42958cd69f7bf6538589ccff68b
OpenWiza <2014800@apps.nsd.org>
2022-05-26 13:49:03 -0700

    Update

--- Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/HighScore.cs
+++ Team-3-Hunt-The-Wumpus/Team-3-Hunt-The-Wumpus/HighScore.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Team_3_Hunt_The_Wumpus
 {
-    class HighScore
+    public class HighScore
     {
-        //hi there
+        public string Name { get; set; }
+        public string Cave { get; set; }
+        public int Score { get; set; }
+
+        public HighScore(string n, string c, int s)
+        {
+            Name = n;
+            Cave = c;
+            Score = s;
+        }
     }
 }
